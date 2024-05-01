755942ce5fff395f6d9295d633d4a8bd70950e86
Kei Murakami <k.murakami0113@gmail.com>
2024-05-01 14:58:01 +0200

    unable translation to save cost for API

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/TranslationService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/TranslationService.java
@@ -15,14 +15,19 @@ public class TranslationService {
     }
 
     public String translateText(String originalText, String targetLanguage) {
+        /**
+         * To save cost for translation API, just returns originalText for now.
+         */
+        return originalText;
+
         // Translates some text into the target language
-        Translation translation = translate.translate(
-                originalText,
-                Translate.TranslateOption.targetLanguage(targetLanguage),
-                // Use "base" for standard model; "nmt" for the Neural Machine Translation model
-                // "base" is cost-effective but with lower quality while NMT is the opposite
-                Translate.TranslateOption.model("base")
-        );
-        return translation.getTranslatedText();
+//        Translation translation = translate.translate(
+//                originalText,
+//                Translate.TranslateOption.targetLanguage(targetLanguage),
+//                // Use "base" for standard model; "nmt" for the Neural Machine Translation model
+//                // "base" is cost-effective but with lower quality while NMT is the opposite
+//                Translate.TranslateOption.model("base")
+//        );
+//        return translation.getTranslatedText();
     }
 }
