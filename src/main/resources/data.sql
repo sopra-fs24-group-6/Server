cade3885587db14ffa29956b56b0880f4f862e06
Aiqi Shuai <mk81472k9@gmail.com>
2024-05-23 12:02:07 +0200

    modifing default users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,10 +9,7 @@ VALUES
     ('user5', 'token5', 1, '555', '2024-01-05', '2000-01-05', 'fr', 22, 22, 0.846, '/images/avatar/good_wolf.png'),
     ('user6', 'token6', 1, '111', '2024-01-06', '2000-01-06', 'es', 38, 12, 2.051, '/images/avatar/hunter.png'),
     ('user7', 'token7', 1, '111', '2024-01-07', '2000-01-07', 'hi', 7, 2, 1.521, '/images/avatar/knight.png'),
-    ('user8', 'token8', 1, '111', '2024-01-08', '2000-01-08', 'ar', 62, 22, 2.158, '/images/avatar/magician.png'),
-    ('user9', 'token9', 1, '111', '2024-01-09', '2000-01-09', 'pt', 108, 2, 2.986, '/images/avatar/nobody.png'),
-    ('user10', 'token10', 1, '111', '2024-01-10', '2000-01-10', 'bn', 24, 5, 2.037, '/images/avatar/ogre.png'),
-    ('user11', 'token11', 1, '111', '2024-01-11', '2000-01-11', 'ru', 89, 43, 2.103, '/images/avatar/suspicious_dude.png');
+    ('user8', 'token8', 1, '111', '2024-01-08', '2000-01-08', 'ar', 62, 22, 2.158, '/images/avatar/magician.png');
 -- Theme
 INSERT INTO THEME (name)
 VALUES
